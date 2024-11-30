5b81795a7e12302fc3748e88d9aae3d0a963a332
Skookuri <alyssa.williams.tk360@gmail.com>
2024-11-30 18:22:52 -0500

    Added abilities turn off on bools but not fully imelemented yet

---
+++ shurikenSagaGame/Assets/Scripts/JutsuType.cs
@@ -0,0 +1,7 @@
+public enum JutsuType
+{
+    Shuriken,  // Corresponds to the ability to throw shuriken
+    Dash,      // Corresponds to the ability to dash
+    PlaneShift, // Corresponds to the ability to plane shift
+    Katana, // Corresponds to the ability to use a katana/sword
+}
--- shurikenSagaGame/Assets/Scripts/Scroll.cs
+++ shurikenSagaGame/Assets/Scripts/Scroll.cs
@@ -8,29 +8,66 @@ public class Scroll : MonoBehaviour
     private DialogueOnCollide collisionTrigger;  // Reference to the DialogueOnCollide script
     public AudioSource ScrollGetSFX;
     public GameObject LevelExitDoor;
+    public JutsuType jutsuTypeUnlocked;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.name == "player") {
+        if (other.gameObject.name == "player" && !isPickedUp)
+        {
             isPickedUp = true;
+
+            // Set the ability based on the unlocked JutsuType
+            UnlockAbility(jutsuTypeUnlocked);
+
+            // Deactivate the scroll after pickup
             gameObject.SetActive(false);
 
             // Find the jutsu object that is the sibling of this scroll
             Transform jutsuTransform = transform.parent.Find("jutsu");
             
-            if (jutsuTransform != null) {
+            if (jutsuTransform != null)
+            {
                 // Enable the jutsuUI display child of jutsu
                 Transform jutsuUI = jutsuTransform.Find("JutsuUI");
-                if (jutsuUI != null) {
+                if (jutsuUI != null)
+                {
                     ScrollGetSFX.Play();
                     jutsuUI.gameObject.SetActive(true);
                     LevelExitDoor.SetActive(false);
-                } else {
+                }
+                else
+                {
                     Debug.LogError("jutsuUI child not found in jutsu.");
                 }
-            } else {
+            }
+            else
+            {
                 Debug.LogError("jutsu object not found as sibling of scroll.");
             }
+        }
+    }
+
+    private void UnlockAbility(JutsuType jutsuType)
+    {
+        // Set the ability flag based on the JutsuTypeUnlocked enum
+        switch (jutsuType)
+        {
+            case JutsuType.Shuriken:
+                Abilities.canShurithrow = true;
+                Debug.Log("Can throw shuriken...");
+                break;
+            case JutsuType.Dash:
+                Abilities.canDash = true;
+                Debug.Log("Can dash...");
+                break;
+            case JutsuType.PlaneShift:
+                Abilities.canShift = true;
+                Debug.Log("Can plane shift...");
+                break;
+            // Add more cases as needed for other jutsu types
+            default:
+                Debug.LogWarning("Unknown JutsuType: " + jutsuType);
+                break;
         }
     }
 }
