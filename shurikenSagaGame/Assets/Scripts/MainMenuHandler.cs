76d1f80617de60614352974e24a42c5f1e5970ac
untitled-Terran <bgiova02@tufts.edu>
2024-12-03 17:29:56 -0500

    finished pause menu

--- shurikenSagaGame/Assets/Scripts/MainMenuHandler.cs
+++ shurikenSagaGame/Assets/Scripts/MainMenuHandler.cs
@@ -56,7 +56,7 @@ public class MainMenuHandler : MonoBehaviour
     }
 
     public void StartGame() {
-        SceneManager.LoadScene("MB Dungeon1");
+        SceneManager.LoadScene("IntroScene");
     }
 
     public void QuitGame() {
